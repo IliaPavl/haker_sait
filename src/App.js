9effbe028361f97832a42b63fa3180d9b4cacbc7
IliaPavl <i.pavl@list.ru>
2023-05-05 13:31:01 +0300

    create Navigaton Bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./routs/AppRouter"
+import NavigationBar from "./components/NavigationBar";
 function App() {
   return (
     <BrowserRouter>
+      <NavigationBar />
       <AppRouter />
     </BrowserRouter>
   );
---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Nav } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import { HOME_PAGE, NEWS_PAGE } from '../const/SaitUrls';
+import './NavigationBar.css';
+
+const NavigationBar = () => {
+    return (
+        <Navbar className='navBar'>
+            <Container>
+                <Navbar.Brand className='navBarBrendText'>
+                    <img
+                        alt=""
+                        src="https://news.ycombinator.com/y18.gif"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />{' '}
+                    Hacker News
+                </Navbar.Brand>
+                <Nav className="me-auto">
+                    <Link className='navbar-brand justify-content-end d-flex nav-item' to={HOME_PAGE}>Home</Link>
+                    <Link className='navbar-brand justify-content-end d-flex nav-item' to={NEWS_PAGE+'/'+1}>News</Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default NavigationBar;
