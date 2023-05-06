c5c02674dd78e97f6f8eb3cd9961eb52c87fbae7
IliaPavl <i.pavl@list.ru>
2023-05-06 11:05:35 +0300

    new styles for News list

--- src/components/ItemListNews.jsx
+++ src/components/ItemListNews.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
+import { Badge, Col, Form, ListGroup, Row } from 'react-bootstrap';
+import './ItemList.css'
 
 const ItemListNews = ({ news }) => {
     return (
-        <div>
-            <div>
-                {news.name}
-            </div>
-            <div>
-                {news.raits}
-            </div>
-            <div>
-                {news.author}
-            </div>
-            <div>
-                {news.date}
-            </div>
-        </div>
+        <ListGroup className='item mb-2'>
+            <ListGroup.Item className="date text-muted">{news.date}</ListGroup.Item>
+            <ListGroup.Item className='itemText'>{news.name}</ListGroup.Item>
+            <ListGroup.Item className=" footer text-muted">
+                Author:
+                <Badge pill bg="success" className='badgeAuthor'>
+                    {news.author}
+                </Badge>
+                Raitig:
+                <Badge bg="info" className='badgeRaiting'>
+                    {news.raits}
+                </Badge>
+            </ListGroup.Item>
+        </ListGroup>
+
     );
 };
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,28 @@
 import React from 'react';
-import { Card, Container } from 'react-bootstrap';
+import { Badge, Card, Col, Container, Row } from 'react-bootstrap';
 import ItemListNews from '../components/ItemListNews';
+import './NewsPage.css'
+import Image from '../images/refresh.png'
 
 const HomePage = () => {
-    const news = [{ name: "news1", raits: "5.5", author: "Nik", date: "12.12.2023" }, { name: "news1", raits: "5.5", author: "Nik", date: "12.12.2023" }, { name: "news1", raits: "5.5", author: "Nik", date: "12.12.2023" }, { name: "news1", raits: "5.5", author: "Nik", date: "12.12.2023" }, { name: "news1", raits: "5.5", author: "Nik", date: "12.12.2023" }]
+    const news = [
+        { name: "Unlimiformer: Long-Range Transformers with Unlimited Length Input", raits: "5.5", author: "Nik", date: "12.12.2023" },
+        { name: "Fulfilling a reader's request for my “dot files”", raits: "5.2", author: "Nik", date: "12.12.2023" },
+        { name: "Waymo One doubles service area in Phoenix, continues growing in SF", raits: "5.5", author: "Nik", date: "12.12.2023" },
+        { name: "news1", raits: "5.5", author: "Nik", date: "12.12.2023" },
+        { name: "news1", raits: "5.5", author: "Nik", date: "12.12.2023" }]
     return (
-        <Container fluid="md" className='mt-3 '>
+        <Container fluid="md" className='mt-3 newsCard'>
             <Card>
-                <Card.Header>
-                    Last news
+                <Card.Header className='newsHeader d-flex flex-row-reverse' >
+                    <div className='imgRefreshDiv'>
+                        <img className='imgRefresh' src={Image} alt='refresh' />
+                    </div>
                 </Card.Header>
                 <Card.Body>
                     {news.map(item => (
                         <ItemListNews news={item} />
                     ))}
-                    body
                 </Card.Body>
             </Card>
         </Container>
