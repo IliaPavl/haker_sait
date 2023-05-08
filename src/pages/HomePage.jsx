217b24213aa493576f297677a6e93f6644301648
IliaPavl <i.pavl@list.ru>
2023-05-08 18:03:31 +0300

    add load by scroll

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -8,20 +8,47 @@ import './NewsPage.css';
 const HomePage = () => {
     const [news, setNews] = useState([]);
     const [page, setPage] = useState([]);
+    const [buferPage, setBuferPage] = useState([]);
+    const [allId, setAllId] = useState([]);
+    const [fetch, setFetch] = useState(false);
+
+    useEffect(() => {
+        document.addEventListener('scroll', scroll);
+        return function () {
+            document.removeEventListener('scroll', scroll);
+        }
+    }, [])
+
+    useEffect(() => {
+        if (fetch) {
+            setBuferPage(allId.splice(page.length, page.length + 1));
+            setPage(prevState => [...prevState, allId.splice(page.length, page.length + 1)])
+            setFetch(false);
+        }
+    }, [fetch])
+    const scroll = (e) => {
+        if (e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 150)
+            setFetch(true);
+    }
 
     function getNewNews() {
         NewsService.getNewNews().then((response) => {
-            setPage(response.data.slice(0, 100))
+            setAllId(response.data.slice(0, 100))
+            setPage(response.data.slice(0, 20))
+            setBuferPage(response.data.slice(0, 20))
         })
+    }
+    function refresh() {
+        setNews([]);
+        getNewNews();
     }
     useEffect(() => {
         getNewNews();
     }, [])
 
-
     useEffect(() => {
-        async function getNews() {
-            page.forEach(id => {
+        async function getBufer() {
+            buferPage.forEach(id => {
                 let promise = new Promise((resolve, reject) => {
                     NewsService.getOneNews(id).then(res => { resolve(res.data); })
                 })
@@ -30,15 +57,14 @@ const HomePage = () => {
                 })
             });
         }
-        setNews([]);
-        getNews();
-    }, [page])
+        getBufer();
+    }, [buferPage])
 
     return (
         <Container fluid="md" className='mt-3 newsCard'>
             <Card>
                 <Card.Header className='newsHeader d-flex flex-row-reverse' >
-                    <div className='imgRefreshDiv' onClick={() => getNewNews()}>
+                    <div className='imgRefreshDiv' onClick={() => refresh()}>
                         <img className='imgRefresh' src={Image} alt='refresh' />
                     </div>
                 </Card.Header>
