40cb68c63750f8a733bab20fe74a116011c2936a
IliaPavl <i.pavl@list.ru>
2023-05-08 15:57:49 +0300

    add get user url

--- src/const/Consts.jsx
+++ src/const/Consts.jsx
@@ -4,5 +4,6 @@ export const NEWS_PAGE = '/news'
 export const json_format = '.json?print=pretty';
 export const SERVER_URL = 'https://hacker-news.firebaseio.com';
 export const BASE_URL = SERVER_URL + '/v0';
-export const NEW_NEWS ='/newstories' + json_format;
+export const NEW_NEWS = '/newstories' + json_format;
 export const BY_ITEM = BASE_URL + '/item';
+export const USER = BASE_URL + '/user';
