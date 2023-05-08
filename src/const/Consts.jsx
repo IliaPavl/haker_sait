4955842f3e170371107c097d1887c6d4f3ce353d
IliaPavl <i.pavl@list.ru>
2023-05-08 12:44:46 +0300

    create const file

---
+++ src/const/Consts.jsx
@@ -0,0 +1,8 @@
+export const HOME_PAGE = '/'
+export const NEWS_PAGE = '/news'
+
+export const json_format = '.json?print=pretty';
+export const SERVER_URL = 'https://hacker-news.firebaseio.com';
+export const BASE_URL = SERVER_URL + '/v0';
+export const NEW_NEWS ='/newstories' + json_format;
+export const BY_ITEM = BASE_URL + '/item';
