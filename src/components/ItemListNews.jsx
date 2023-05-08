f626da0832dbee2f6a25ac0dcc414c44099f7f79
IliaPavl <i.pavl@list.ru>
2023-05-08 12:57:49 +0300

    add navigate go to news/id

--- src/components/ItemListNews.jsx
+++ src/components/ItemListNews.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Badge, ListGroup } from 'react-bootstrap';
 import './ItemList.css';
+import { useNavigate } from 'react-router-dom';
+import { NEWS_PAGE } from '../const/Consts';
 
 const ItemListNews = ({ news }) => {
     const [day, setDay] = useState('');
@@ -8,7 +10,7 @@ const ItemListNews = ({ news }) => {
     const [year, setYear] = useState('');
     const [minutes, setMinutes] = useState('');
     const [hourse, setHourse] = useState('');
-
+    const navigate =useNavigate();
     useEffect(() => {
         function setParse(val) {
             if (val < 10)
@@ -23,12 +25,16 @@ const ItemListNews = ({ news }) => {
             setMonth(setParse(date.getMonth()));
             setHourse(setParse(date.getHours()));
         }
-
     }, [news])
+
+    function goTo(id){
+        navigate(NEWS_PAGE+'/'+id);
+    }
+
     return (
         <ListGroup className='item mb-2'>
             <ListGroup.Item className="date text-muted">{hourse}:{minutes} {day}.{month}.{year} </ListGroup.Item>
-            <ListGroup.Item className='itemText'>{news.title}</ListGroup.Item>
+            <ListGroup.Item className='itemText' onClick={()=>goTo(news.id)}>{news.title}</ListGroup.Item>
             <ListGroup.Item className=" footer text-muted">
                 Author:
                 <Badge pill bg="success" className='badgeAuthor'>
