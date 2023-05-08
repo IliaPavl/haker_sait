144d15e8b0e784ac65ff7bd190719149562906a7
IliaPavl <i.pavl@list.ru>
2023-05-08 12:44:37 +0300

    create itemls for list news

--- src/components/ItemListNews.jsx
+++ src/components/ItemListNews.jsx
@@ -1,20 +1,42 @@
-import React from 'react';
-import { Badge, Col, Form, ListGroup, Row } from 'react-bootstrap';
-import './ItemList.css'
+import React, { useEffect, useState } from 'react';
+import { Badge, ListGroup } from 'react-bootstrap';
+import './ItemList.css';
 
 const ItemListNews = ({ news }) => {
+    const [day, setDay] = useState('');
+    const [month, setMonth] = useState('');
+    const [year, setYear] = useState('');
+    const [minutes, setMinutes] = useState('');
+    const [hourse, setHourse] = useState('');
+
+    useEffect(() => {
+        function setParse(val) {
+            if (val < 10)
+                return '0' + val;
+            return val;
+        }
+        if (news !== undefined) {
+            let date = new Date(news.time * 1000);
+            setMinutes(setParse(date.getMinutes()))
+            setDay(setParse(date.getDay()));
+            setYear(date.getFullYear());
+            setMonth(setParse(date.getMonth()));
+            setHourse(setParse(date.getHours()));
+        }
+
+    }, [news])
     return (
         <ListGroup className='item mb-2'>
-            <ListGroup.Item className="date text-muted">{news.date}</ListGroup.Item>
-            <ListGroup.Item className='itemText'>{news.name}</ListGroup.Item>
+            <ListGroup.Item className="date text-muted">{hourse}:{minutes} {day}.{month}.{year} </ListGroup.Item>
+            <ListGroup.Item className='itemText'>{news.title}</ListGroup.Item>
             <ListGroup.Item className=" footer text-muted">
                 Author:
                 <Badge pill bg="success" className='badgeAuthor'>
-                    {news.author}
+                    {news.by}
                 </Badge>
                 Raitig:
                 <Badge bg="info" className='badgeRaiting'>
-                    {news.raits}
+                    {news.score}
                 </Badge>
             </ListGroup.Item>
         </ListGroup>
