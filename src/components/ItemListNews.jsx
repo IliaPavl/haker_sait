3c8eb55896ce1a74492a3b6b9998d990f9391292
IliaPavl <i.pavl@list.ru>
2023-05-08 15:56:35 +0300

    refactor add more Badges and List items

--- src/components/ItemListNews.jsx
+++ src/components/ItemListNews.jsx
@@ -1,49 +1,54 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Badge, ListGroup } from 'react-bootstrap';
-import './ItemList.css';
 import { useNavigate } from 'react-router-dom';
 import { NEWS_PAGE } from '../const/Consts';
+import DateLable from './DateLable';
+import './ItemList.css';
 
-const ItemListNews = ({ news }) => {
-    const [day, setDay] = useState('');
-    const [month, setMonth] = useState('');
-    const [year, setYear] = useState('');
-    const [minutes, setMinutes] = useState('');
-    const [hourse, setHourse] = useState('');
-    const navigate =useNavigate();
-    useEffect(() => {
-        function setParse(val) {
-            if (val < 10)
-                return '0' + val;
-            return val;
-        }
-        if (news !== undefined) {
-            let date = new Date(news.time * 1000);
-            setMinutes(setParse(date.getMinutes()))
-            setDay(setParse(date.getDay()));
-            setYear(date.getFullYear());
-            setMonth(setParse(date.getMonth()));
-            setHourse(setParse(date.getHours()));
-        }
-    }, [news])
+const ItemListNews = ({ news, saitLink, description, commentsArray }) => {
+    const link = useState(saitLink);
+    const text = useState(description);
+    const comments = useState(commentsArray);
+    const navigate = useNavigate();
 
-    function goTo(id){
-        navigate(NEWS_PAGE+'/'+id);
+    function goTo(id) {
+        if (link === undefined)
+            navigate(NEWS_PAGE + '/' + id);
+        else
+            window.location.href = link;
     }
+
+    function createMarkup(text) { return { __html: text }; };
+
 
     return (
         <ListGroup className='item mb-2'>
-            <ListGroup.Item className="date text-muted">{hourse}:{minutes} {day}.{month}.{year} </ListGroup.Item>
-            <ListGroup.Item className='itemText' onClick={()=>goTo(news.id)}>{news.title}</ListGroup.Item>
+            <ListGroup.Item className="date text-muted">
+                <DateLable numberDate={news !== undefined ? news.time : undefined} />
+            </ListGroup.Item>
+            <ListGroup.Item className='itemText' onClick={() => goTo(news.id)}>
+                {news !== undefined ? news.title : ""}
+            </ListGroup.Item>
+            {text !== undefined ?
+                <ListGroup.Item className=" footer text-muted">
+                    <div dangerouslySetInnerHTML={createMarkup(text)} />
+                </ListGroup.Item> : <></>}
             <ListGroup.Item className=" footer text-muted">
                 Author:
-                <Badge pill bg="success" className='badgeAuthor'>
-                    {news.by}
+                <Badge pill bg="success" className='badge badgeAuthor'>
+                    {news !== undefined ? news.by : ""}
                 </Badge>
                 Raitig:
-                <Badge bg="info" className='badgeRaiting'>
-                    {news.score}
+                <Badge bg="info" className='badge'>
+                    {news !== undefined ? news.score : ""}
                 </Badge>
+                {comments !== undefined ?
+                    <>Comments: <Badge bg="warning" className='badge'>{comments}</Badge> </>
+                    : <></>}
+                {link !== undefined ?
+                    <a href={link}>
+                        <Badge className='badge'>Link</Badge>
+                    </a> : <></>}
             </ListGroup.Item>
         </ListGroup>
 
